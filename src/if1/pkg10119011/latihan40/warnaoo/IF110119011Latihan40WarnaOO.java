21919583a56cfd1dc17a1122755a079e780bbd58
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-11-07 11:37:21 +0700

    IF1-10119011-Latihan40-WarnaOO

---
+++ src/if1/pkg10119011/latihan40/warnaoo/IF110119011Latihan40WarnaOO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan40.warnaoo;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class IF110119011Latihan40WarnaOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        WarnaOO wrn = new WarnaOO();
+        wrn.inputWarna();
+        wrn.inputNama();
+        wrn.cekWarna();
+    }
+    
+}
